8055ef9de8b742b95a8d0cb47dbf362ab34ae733
sandeeppawar15 <sandeeppawar.pawar15@gmail.com>
2022-08-14 00:10:37 +0530

    Bar chart - stacked option implementation - In progress

--- src/app/chart/barchart/barchart.component.ts
+++ src/app/chart/barchart/barchart.component.ts
@@ -12,18 +12,41 @@ export class BarchartComponent implements OnInit {
 
   public barChartLegend = true;
   public barChartPlugins = [];
-
+  maxLimit: number = 70;
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: ['2022 Jan', '2022 Feb', '2022 Mar', '2022 Apr', '2022 May', '2022 June', '2022 July', '2022 Aug', '2022 Sept', '2022 Oct', '2022 Nov', '2022 Dec'],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Bestbuy' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Walmart' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Samsung' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Sams Club' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Target' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Google' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Walmart Dotcom' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'ATB' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Apple' },
+      { data: [Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit), Math.round(Math.random() * this.maxLimit)], label: 'Microsoft' }
+
+
     ]
   };
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
   };
+
+  // public barChartOptions: any = {
+  //   scaleShowVerticalLines: false,
+  //   responsive: true,
+  //   scales: {
+  //     xAxes: [{
+  //       stacked: true
+  //     }],
+  //     yAxes: [{
+  //       stacked: true
+  //     }]
+  //   }
+  // };
 
   constructor() { }
 
